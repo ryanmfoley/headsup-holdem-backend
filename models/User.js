94923e23e55787c3b801794e5566681bc9e8ba21
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-27 18:44:22 -0700

    Build User Schema

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('../db/connection')
+const { Model, Schema } = mongoose
+
+// Create Schema //
+const UserSchema = new Schema({
+	username: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+})
+
+const User = model('User', UserSchema)
+
+module.exports = User
