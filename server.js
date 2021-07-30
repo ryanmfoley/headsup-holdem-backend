144a784d5070e8bfa4274cb9cb851e1aef1611a7
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-29 20:32:43 -0700

    Socket.io is setup on server allowing players to start a game from lobby

--- server.js
+++ server.js
@@ -21,6 +21,51 @@ require('./config/passport')(passport)
 // Controllers //
 app.use('/api/users', require('./controllers/users'))
 
+//______________________________________________________________
+// START SOCKET CONNECTION HERE
+
+// Run when client connects //
+io.on('connection', (socket) => {
+	const { Player, addPlayer, removePlayer } = require('./utils/players')
+
+	let playersWaiting = []
+
+	// Send player data and waiting list to client //
+	socket.on('enterLobby', (username) => {
+		const player = new Player(socket.id, username)
+
+		socket.emit('enterLobby', player)
+		socket.emit('playersWaiting', playersWaiting)
+	})
+
+	socket.on('createGame', (player) => {
+		// Add player to waiting list //
+		playersWaiting = addPlayer(playersWaiting, player)
+
+		// Send list of players waiting to client //
+		io.emit('playersWaiting', playersWaiting)
+	})
+
+	socket.on('joinGame', (id) => {
+		// Join socket to a given room //
+		socket.join(id)
+	})
+
+	socket.on('updatePlayersWaiting', (id) => {
+		// Remove player from waiting list //
+		playersWaiting = removePlayer(playersWaiting, id)
+
+		io.emit('playersWaiting', playersWaiting)
+	})
+
+	socket.on('logout', (id) => {
+		// Remove player from waiting list //
+		playersWaiting = removePlayer(playersWaiting, id)
+
+		io.emit('playersWaiting', playersWaiting)
+	})
+})
+
 const { PORT } = process.env
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ utils/players.js
@@ -0,0 +1,24 @@
+function Player(id, username, room = id) {
+	this.id = id
+	this.username = username
+	this.room = room
+}
+
+const addPlayer = (playersWaiting, newPlayer) => {
+	if (
+		!playersWaiting.find((player) => player.username === newPlayer.username)
+	) {
+		playersWaiting.push(newPlayer)
+	}
+
+	return playersWaiting
+}
+
+const removePlayer = (playersWaiting, id) =>
+	playersWaiting.filter((player) => player.id !== id)
+
+module.exports = {
+	Player,
+	addPlayer,
+	removePlayer,
+}
