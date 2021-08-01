a6f46c5fea9d8e046fdce2bf51372b188cc1e53b
ryfoa6 <ryanmfoley84@gmail.com>
2021-08-01 00:22:15 -0700

    Finish dealer methods for analyzing hand rankings

--- utils/dealer.js
+++ utils/dealer.js
@@ -75,25 +75,45 @@ class Dealer {
 		const isBroadWay = hand.every((card) => card.value >= 10)
 		const isRoyalFlush = isBroadway && this.isFlush(hand)
 
-		const handValue = isRoyalFlush ? 1000 : 0
+		const handValue = isRoyalFlush ? 10000 : 0
 	}
 
 	isStraightFlush(hand) {
 		const isStraightFlush = this.isStraight(hand) && this.isFlush(hand)
 
-		const handValue = isStraightFlush ? 500 : 0
+		const handValue = isStraightFlush ? 7000 : 0
+	}
+
+	isFourOfAKind(hand) {
+		const cardCounts = {}
+
+		// Count occurences of each rank //
+		hand.forEach(({ rank }) => (cardCounts[rank] = (cardCounts[rank] || 0) + 1))
+
+		const counts = Object.values(cardCounts)
+		const isFourOfAKind = counts.includes(4)
+		const handValue = isFourOfAKind ? 5000 : 0
+
+		return handValue
 	}
 
 	isFullHouse(hand) {
-		let playersHandValue
+		const cardCounts = {}
 
-		return playersHandValue
+		// Count occurences of each rank //
+		hand.forEach(({ rank }) => (cardCounts[rank] = (cardCounts[rank] || 0) + 1))
+
+		const counts = Object.values(cardCounts)
+		const isFullHouse = counts.includes(2) && counts.includes(3)
+		const handValue = isFullHouse ? 3000 : 0
+
+		return handValue
 	}
 
 	isFlush(hand) {
 		const isFlush = hand.every((card) => card.suit === hand[0].suit)
 
-		const handValue = isFlush ? 200 : 0
+		const handValue = isFlush ? 1500 : 0
 
 		return handValue
 	}
@@ -125,39 +145,62 @@ class Dealer {
 			isStraight = allConsecutives(sortedHand)
 		}
 
-		const handValue = isStraight ? 100 : 0
+		const handValue = isStraight ? 1000 : 0
 
 		return handValue
 	}
 
 	isTrips(hand) {
-		let playersHandValue
+		const cardCounts = {}
 
-		return playersHandValue
+		// Count occurences of each rank //
+		hand.forEach(({ rank }) => (cardCounts[rank] = (cardCounts[rank] || 0) + 1))
+
+		const counts = Object.values(cardCounts)
+		const isTrips = counts.includes(3)
+		const handValue = isTrips ? 700 : 0
+
+		return handValue
 	}
 
 	isTwoPair(hand) {
-		let playersHandValue
+		const cardCounts = {}
 
-		return playersHandValue
+		// Count occurences of each rank //
+		hand.forEach(({ rank }) => (cardCounts[rank] = (cardCounts[rank] || 0) + 1))
+
+		const counts = Object.values(cardCounts)
+		const isTwoPair = counts.filter((card) => card === 2).length === 2
+		const handValue = isTwoPair ? 200 : 0
+
+		return handValue
 	}
 
 	isPair(hand) {
-		let playersHandValue
+		const cardCounts = {}
 
-		return playersHandValue
+		// Count occurences of each rank //
+		hand.forEach(({ rank }) => (cardCounts[rank] = (cardCounts[rank] || 0) + 1))
+
+		const counts = Object.values(cardCounts)
+		const isPair = counts.includes(2)
+		const handValue = isPair ? 100 : 0
+
+		return handValue
 	}
 
 	isHighCard(hand) {
-		let playersHandValue
+		const cardValues = hand.map(({ value }) => value)
+		const handValue = Math.max.apply(null, cardValues)
 
-		return playersHandValue
+		return handValue
 	}
 
 	calculateHandValues(playerOnesHand, playerTwosHand) {
 		const playerOnesHandValue =
 			this.isRoyalFlush(playerOnesHand) ||
 			this.isStraightFlush(playerOnesHand) ||
+			this.isFourOfAKind(playerOnesHand) ||
 			this.isFullHouse(playerOnesHand) ||
 			this.isFlush(playerOnesHand) ||
 			this.isStraight(playerOnesHand) ||
@@ -169,6 +212,7 @@ class Dealer {
 		const playerTwosHandValue =
 			this.isRoyalFlush(playerTwosHand) ||
 			this.isStraightFlush(playerTwosHand) ||
+			this.isFourOfAKind(playerTwosHand) ||
 			this.isFullHouse(playerTwosHand) ||
 			this.isFlush(playerTwosHand) ||
 			this.isStraight(playerTwosHand) ||
